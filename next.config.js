4a2dd2e465f2f7bb90b2ce07bfb76ac3db25bc98
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2023-04-01 13:56:56 -0600

    slint y prettier configurados

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    customKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: '/admin/articulos',
+        permanent: true,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
